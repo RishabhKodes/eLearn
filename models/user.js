12fb89f38b048ed6af7a7222252a8cc1d961ed8f
RishabhKodes <rishabhbhandari6@gmail.com>
2020-12-31 21:42:55 +0530

    final commit v1

--- models/user.js
+++ models/user.js
@@ -1,51 +1,62 @@
 var mongoose = require('mongoose');
 var bcrypt = require('bcryptjs');
 
+// User Schema
 var UserSchema = mongoose.Schema({
-    username:{type:String},
-    email:{type:String},
-    password:{type:String, bcrypt: true},
-    type:{type:String}
+	username: {
+		type: String
+	},
+	email: {
+		type: String
+	},
+	password:{
+		type:String,
+		bcrypt: true
+	},
+	type:{
+		type:String
+	}
+	
 });
 
 var User = module.exports = mongoose.model('User', UserSchema);
 
-//get single user data by id
+// Get User By Id
 module.exports.getUserById = function(id, callback){
-    User.findById(id, callback).lean();
+	User.findById(id, callback);
 }
 
-//get single user data by username
+// Get User by Username
 module.exports.getUserByUsername = function(username, callback){
-    var query = {username: username};
-    User.findOne(query, callback).lean();
-}
-
-//create student user
-module.exports.saveStudent = function(newUser, newStudent, callback){
-    bcrypt.hash(newUser.password, 10, function(err, hash){
-        if(err) throw err;
-
-        newUser.password = hash;
-        console.log('Student data being saved!');
-        async.parallel([newUser.save, newStudent.save], callback);
-    });
-}
-
-//create instructor user
-module.exports.saveInstructor = function(newUser, newInstructor, callback){
-    bcrypt.hash(newUser.password, 10, function(err, hash){
-        if(err) throw err;
-
-        newUser.password = hash;
-        console.log('Instructor data being saved!');
-        async.parallel([newUser.save, newInstructor.save], callback);
-    });
+	var query = {username: username};
+	User.findOne(query, callback);
 }
 
 module.exports.comparePassword = function(candidatePassword, hash, callback){
-    bcrypt.compare(candidatePassword, hash, function(err, isMatch){
-        if(err) throw err;
-        callback(null, isMatch);
-    })
+	bcrypt.compare(candidatePassword, hash, function(err, isMatch){
+		if(err) throw err;
+		callback(null, isMatch);
+	});
+}
+
+// Create Student User
+module.exports.saveStudent = function(newUser, newStudent, callback){
+	bcrypt.hash(newUser.password, 10, function(err, hash){
+		if(err) throw errl
+		// Set hash
+		newUser.password = hash;
+		console.log('Student is being saved');
+		async.parallel([newUser.save, newStudent.save], callback);
+	});
+}
+
+// Create Instructor User
+module.exports.saveInstructor = function(newUser, newInstructor, callback){
+	bcrypt.hash(newUser.password, 10, function(err, hash){
+		if(err) throw errl
+		// Set hash
+		newUser.password = hash;
+		console.log('Instructor is being saved');
+		async.parallel([newUser.save, newInstructor.save], callback);
+	});
 }
